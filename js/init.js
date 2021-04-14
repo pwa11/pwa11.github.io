d9c597c1613cdff763e9b90063ce4304606c918c
pwa11 <82481726+pwa11@users.noreply.github.com>
2021-04-14 03:44:45 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDjxeWJaFhrNdK6VLd3W9IeGP44I5JUjYk",
+  authDomain: "pwa11-36b85.firebaseapp.com",
+  projectId: "pwa11-36b85",
+  storageBucket: "pwa11-36b85.appspot.com",
+  messagingSenderId: "21288380442",
+  appId: "1:21288380442:web:64bab5e9c2b43e98d2c9a3",
+  measurementId: "G-F8BSBBFNBG"
+});
